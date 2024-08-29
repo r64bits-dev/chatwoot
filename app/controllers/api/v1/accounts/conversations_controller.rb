2c7dccc27452b094967e5d94f845f31aff333773
Daniel Machado <danielmachadopintos@gmail.com>
2024-08-29 16:45:42 -0300

    feat: add conversation controller

--- app/controllers/api/v1/accounts/conversations_controller.rb
+++ app/controllers/api/v1/accounts/conversations_controller.rb
@@ -96,8 +96,10 @@ class Api::V1::Accounts::ConversationsController < Api::V1::Accounts::BaseContro
   end
 
   def whatsapp_groups_participants
-    # res = Evolution::FindParticipantsService.new(@conversation.inbox.name, @conversation.contact.identifier).perform
-    render json: Evolution::FindParticipantsService.new('5541991179192', '120363278888180777%40g.us').perform
+    render json: { error: 'Invalid inbox type' }, status: :unprocessable_entity if @conversation.inbox.whatsapp?
+    render json: { error: 'Invalid contact identifier' }, status: :unprocessable_entity if @conversation.contact.identifier.blank?
+
+    render json: Evolution::FindParticipantsService.new(@conversation.inbox.name, @conversation.contact.identifier).perform
   end
 
   private

f07f7ddd6b6a93e6de1ab92a701c01f3a966bcc7
Daniel Machado <danielmachadopintos@gmail.com>
2024-09-10 12:40:11 -0300

    fix: try another attempt

--- app/controllers/webhooks/instagram_controller.rb
+++ app/controllers/webhooks/instagram_controller.rb
@@ -1,15 +1,11 @@
 class Webhooks::InstagramController < ActionController::API
   include MetaTokenVerifyConcern
 
-  MUTEX = Mutex.new
-
   def events
     Rails.logger.info('Instagram webhook received events')
 
     if params['object'].casecmp('instagram').zero?
-      MUTEX.synchronize do
-        ::Webhooks::InstagramEventsJob.perform_later(params.to_unsafe_hash[:entry])
-      end
+      ::Webhooks::InstagramEventsJob.perform_later(params.to_unsafe_hash[:entry])
       render json: :ok
     else
       Rails.logger.warn("Message is not received from the instagram webhook event: #{params['object']}")

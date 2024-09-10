b8fdc428074a44dc8f513c2ec81c04ee3c6b7a62
Daniel Machado <danielmachadopintos@gmail.com>
2024-09-10 11:42:38 -0300

    fix: attempt to try remove duplicate message

--- app/controllers/webhooks/instagram_controller.rb
+++ app/controllers/webhooks/instagram_controller.rb
@@ -1,10 +1,15 @@
 class Webhooks::InstagramController < ActionController::API
   include MetaTokenVerifyConcern
 
+  MUTEX = Mutex.new
+
   def events
     Rails.logger.info('Instagram webhook received events')
+
     if params['object'].casecmp('instagram').zero?
-      ::Webhooks::InstagramEventsJob.perform_later(params.to_unsafe_hash[:entry])
+      MUTEX.synchronize do
+        ::Webhooks::InstagramEventsJob.perform_later(params.to_unsafe_hash[:entry])
+      end
       render json: :ok
     else
       Rails.logger.warn("Message is not received from the instagram webhook event: #{params['object']}")

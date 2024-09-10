c37d82415c4d74989e92bb984489c8ca48929b10
Daniel Machado <danielmachadopintos@gmail.com>
2024-09-10 15:00:50 -0300

    feat: more logs

--- app/controllers/webhooks/instagram_controller.rb
+++ app/controllers/webhooks/instagram_controller.rb
@@ -4,7 +4,7 @@ class Webhooks::InstagramController < ActionController::API
   def events
     Rails.logger.info('Instagram webhook received events')
 
-    p request
+    p request.headers
     if params['object'].casecmp('instagram').zero?
       ::Webhooks::InstagramEventsJob.perform_later(params.to_unsafe_hash[:entry])
       render json: :ok

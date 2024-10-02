2f37444567e253d9427f9b18774ec60f028845f9
Daniel Machado <danielmachadopintos@gmail.com>
2024-10-02 11:19:54 -0300

    fix: application develop

--- config/application.rb
+++ config/application.rb
@@ -47,9 +47,12 @@ module Chatwoot
 
     # Custom chatwoot configurations
     config.x = config_for(:app).with_indifferent_access
-
-    # default locale
-    config.i18n.default_locale = :'pt-BR'
+    print('avaliable locales: ', I18n.available_locales)
+    config.i18n.default_locale = if I18n.available_locales.include?('pt-BR')
+                                   :'pt-BR'
+                                 else
+                                   :pt
+                                 end
 
     # https://stackoverflow.com/questions/72970170/upgrading-to-rails-6-1-6-1-causes-psychdisallowedclass-tried-to-load-unspecif
     # https://discuss.rubyonrails.org/t/cve-2022-32224-possible-rce-escalation-bug-with-serialized-columns-in-active-record/81017

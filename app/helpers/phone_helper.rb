3c27109a673418341ddb70680ff6cce6e499fda8
Daniel Machado <danielmachadopintos@gmail.com>
2024-10-07 17:13:46 -0300

    feat: whatss events job

--- app/helpers/phone_helper.rb
+++ app/helpers/phone_helper.rb
@@ -3,7 +3,9 @@ module PhoneHelper
     return phone_number if phone_number.blank?
 
     phone_number.gsub!(/[^\d]/, '')
-    phone_number.gsub!(/^(\d{3})(\d{3})(\d{4})$/, '\\1-\\2-\\3')
+    phone_number = "55#{phone_number}" if !phone_number.start_with?('55') && (phone_number.length == 10 || phone_number.length == 11)
+    phone_number = "+#{phone_number}" unless phone_number.start_with?('+')
+
     phone_number
   end
 end

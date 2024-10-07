ede317554e7ea11fff48045612d8225eed2795a8
Daniel Machado <danielmachadopintos@gmail.com>
2024-10-07 11:55:12 -0300

    feat: add put log

--- app/jobs/event_dispatcher_job.rb
+++ app/jobs/event_dispatcher_job.rb
@@ -2,6 +2,7 @@ class EventDispatcherJob < ApplicationJob
   queue_as :critical
 
   def perform(event_name, timestamp, data)
+    p "event name, #{event_name}, timestamp, #{timestamp}, data, #{data}"
     Rails.configuration.dispatcher.async_dispatcher.publish_event(event_name, timestamp, data)
   end
 end

bb7eca34db1bdc5c055cfaef05bbd5f5020dca33
Daniel Machado <danielmachadopintos@gmail.com>
2024-10-08 09:29:25 -0300

    fix: more logs

--- app/services/whatsapp/providers/whatsapp_360_dialog_service.rb
+++ app/services/whatsapp/providers/whatsapp_360_dialog_service.rb
@@ -27,6 +27,7 @@ class Whatsapp::Providers::Whatsapp360DialogService < Whatsapp::Providers::BaseS
     # ensuring that channels with wrong provider config wouldn't keep trying to sync templates
     whatsapp_channel.mark_message_templates_updated
     response = HTTParty.get("#{api_base_path}/configs/templates", headers: api_headers)
+    p 'response', response
     whatsapp_channel.update(message_templates: response['waba_templates'], message_templates_last_updated: Time.now.utc) if response.success?
   end
 
